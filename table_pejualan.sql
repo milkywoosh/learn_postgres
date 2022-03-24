f86cb8e1772f2fa39fed45e527f6030cb949565b
lukmanx <archer.lukman@gmail.com>
2022-03-24 22:28:32 +0700

    add relation

--- table_pejualan.sql
+++ table_pejualan.sql
@@ -27,7 +27,6 @@ CREATE TABLE pesan (
     REFERENCES pelanggan (id_pelanggan)
 );
 
-
 CREATE TABLE detil_pesan (
 	id_detil_pesan INT NOT NULL,
     id_pesan INT NOT NULL,
@@ -38,4 +37,32 @@ CREATE TABLE detil_pesan (
     FOREIGN KEY (id_produk) REFERENCES produk (id_produk),
     FOREIGN KEY(id_pesan) REFERENCES pesan (id_pesan)
 );
+
+INSERT INTO pelanggan (id_pelanggan, nm_pelanggan, alamat, telepon, email)
+VALUES 
+('P001', 'beni', 'jalan mambo', 080001, 'beni@mail.com'),
+('P002', 'roni', 'jalan mendil', 082222, 'roni@mail.com'),
+('P003', 'yun', 'jalan mendol', 082332, 'yun@mail.com');
+
+INSERT INTO produk (id_produk, nm_produk, satuan, harga, stock)
+VALUES 
+('C001', 'milkita', 'gram', 100, 50),
+('C002', 'indomie', 'gram', 12, 10),
+('C003', 'jeruk', 'gram', 20000, 10);
+
+INSERT INTO pesan (id_pelanggan, tgl_pesan)
+VALUES ('P001', '2022-01-03'),
+('P001', '2022-02-03'),
+('P003', '2022-01-04'),
+('P003', '2022-01-08');
+
+-- GROUP BY pelanggan.id_pelanggan
+SELECT pelanggan.id_pelanggan, pelanggan.nm_pelanggan, count( pelanggan.nm_pelanggan)as jml
+FROM pesan LEFT JOIN pelanggan
+ON pelanggan.id_pelanggan = pesan.id_pelanggan
+GROUP BY pelanggan.id_pelanggan;
+
+
+
+
 
