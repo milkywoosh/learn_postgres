f9feba9a6c56f47dfc9a399279ce1830ed7e1ca1
lukmanx <archer.lukman@gmail.com>
2022-04-10 01:05:31 +0700

    Update merge data from csv/excel file

--- apl_project/apl_project.sql
+++ apl_project/apl_project.sql
@@ -16,24 +16,24 @@ VALUES('MEGA BANK', 'CGK000504'),
 ('OVO', 'CGK053640')
 
 -- CREATE TABLE
-CREATE TABLE mega_cgk000504
+CREATE TABLE mega
 (
 	id SERIAL,
     awb VARCHAR(30) NOT NULL UNIQUE,
- 	pelanggan VARCHAR (100) NOT NULL,
- 	penerima VARCHAR(100) NOT NULL,
- 	Tlp1 VARCHAR(20) DEFAULT NULL,
+ 	pelanggan VARCHAR (100) ,
+ 	penerima VARCHAR(100) ,
+ 	Tlp1 VARCHAR(20) ,
  	no_referensi text ,
- 	tanggal date NOT NULL,
- 	tlc_tujuan VARCHAR(3) NOT NULL,
- 	tujuan VARCHAR(90) NOT NULL,
+ 	tanggal date ,
+ 	tlc_tujuan VARCHAR(3) ,
+ 	tujuan VARCHAR(90) ,
  	ktp VARCHAR(2) DEFAULT 'X',
  	kirim_ke_client date,
- 	status_pod VARCHAR(12) NOT NULL,
+ 	status_pod VARCHAR(12) ,
  	kasus text DEFAULT 'X',
  	keterangan text DEFAULT 'X',
- 	tanggal_terima date NOT NULL,
-	wilayah VARCHAR(20) NOT NULL,
+ 	tanggal_terima date ,
+	wilayah VARCHAR(20) ,
  	PRIMARY KEY(id)
 );
 
@@ -49,8 +49,9 @@ DELIMITER ';' CSV HEADER;
 PREPARE DATA EXCEL --> SAVE AS CSV, DONT OPEN AGAIN FROM EXCEL
 JUST FROM TEXT EDITOR
 -- IMPORT DATA FROM CSV TO POSTGERS
-COPY mega_cgk000504(awb,pelanggan,penerima,tlp1,no_referensi,tanggal,tlc_tujuan,tujuan,ktp,kirim_ke_client,status_pod,kasus,keterangan,tanggal_terima,wilayah) 
+COPY mega(awb,pelanggan,penerima,tlp1,no_referensi,tanggal,tlc_tujuan,tujuan,ktp,kirim_ke_client,status_pod,kasus,keterangan,tanggal_terima,wilayah) 
 FROM '/Users/macbook/learn_postgres/apl_project/bulkmega.csv' DELIMITER ';' csv HEADER;
+
 
 
 -- QUERY MULTIPLE ID
@@ -70,3 +71,37 @@ WHERE awb in (
 );
 
 EDIT LANGSUNG DARI CELL POSTGRES CLICK 2X --> SAVE F6;
+
+-- INSERT VALUES AND DO NOTHING/CONTINUE when FIND DUPLICATE VALUE
+INSERT INTO countries (country) VALUES ('France'),('Japan') ON CONFLICT (country) DO NOTHING;
+
+INSERT INTO mega (awb)
+VALUES ('CGK8078259901039'),
+('CGK8078259901584'),
+('CGK8078259901591'),
+('CGK8078259901600'),
+('CGK8078259901611'),
+('CGK8078259901624'),
+('CGK8078259901639'),
+('CGK8078259901646'),
+('CGK8078259901655'),
+('CGK8078259901666'),
+('CGK8078259901679'),
+('CGK8078259901684'),
+('CGK8078259901691'),
+('CGK8078259901700'),
+('CGK8078259901711')
+ON CONFLICT (awb) DO NOTHING;
+
+
+
+-- MERGING DATA TO PG FROM CSV FILE: REF: https://stackoverflow.com/questions/8910494/how-to-update-selected-rows-with-values-from-a-csv-file-in-postgres
+CREATE TEMP TABLE tmp_x (awb VARCHAR(20), penerima VARCHAR(100));
+SELECT * from tmp_x;
+COPY tmp_x(awb, penerima) FROM '/Users/macbook/learn_postgres/apl_project/forupdate_mega.csv' DELIMITER ';' CSV HEADER;
+
+-- UPDATE DATA FROM DATA excel/csv
+UPDATE mega
+SET penerima = tmp_x.penerima
+FROM tmp_x
+WHERE mega.awb = tmp_x.awb;
